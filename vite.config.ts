ce7d32adb2d12fd470392971d2956a355b3c67b4
Ryuqg <skv92571@gmail.com>
2025-11-16 15:49:23 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  define: {
+    __BASE_PATH__: JSON.stringify('/')
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
+})
